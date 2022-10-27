197c0c895727381c0e2dc15adac2d8cbc9c38343
Renan Lopes Cavalcante <renanlc1857@gmail.com>
2022-10-26 23:56:20 -0300

    Treino simples de Return/Def

---
+++ funcoes.py
@@ -0,0 +1,5 @@
+def velocidade_media(distancia, tempo):
+    print(f'A velocidade média de {distancia} por {tempo} é de {distancia/tempo} m/s.')
+    return distancia/tempo
+
+velocidade_media(100,25)
